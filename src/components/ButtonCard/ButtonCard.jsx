f8222a7282541675d97baa66e877da56113faa2a
Hugo Silva <hugofranciscodasilva@gmail.com>
2026-02-17 10:24:44 -0300

    Ajustes no buttonCard

--- src/components/ButtonCard/ButtonCard.jsx
+++ src/components/ButtonCard/ButtonCard.jsx
@@ -1,16 +1,19 @@
 
-
 function ButtonCard(props){
+
+    const temIcone = props.icon && props.icon !== ''
+    const statusEdicao = props.status !== "Em andamento"
+
     return(
-        <div className="w-80 m-auto p-2 shadow-lg rounded-sm shadow flex justify-between">
-            <div>
+        <div className="w-80 mx-auto my-5 shadow-lg rounded-md flex justify-between items-center p-5 ">
+            <div className={`${temIcone ? 'block' : 'hidden'}`}>
                 <img className="invert" src={props.icon} alt={props.alt} />
             </div>
             <div>
-                <h2 className="text-lg">{props.texto}</h2>
+                <h2 className="text-lg font-bold">{props.texto}</h2>
                 <p className="text-gray-400 text-sm">({props.status})</p>
             </div>
-            <div className="">
+            <div className={`${statusEdicao ? 'hidden' : 'block'}`}>
                 <img src="/icons/seta-icon.svg" alt="Seta" />
             </div>
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,15 +4,20 @@ import Botao from "../components/Botao/Botao"
 import NavBar from "../components/NavBar/NavBar"
 import ButtonCard from "../components/ButtonCard/ButtonCard"
 
-function Home(){
-    return(
+function Home() {
+    return (
         < >
-        <BannerGlobal />
-        <Link to={"/NovaEdicao"}>
-            <Botao> <img src="/icons/plus-icon.svg" alt="Mais" /> Criar Nova edição</Botao>
-        </Link>
-        <ButtonCard texto="Interclasse 2026" status="Em andamento" icon="/icons/house-icon.svg"/>
-        <NavBar />
+            <BannerGlobal />
+            <Link to={"/NovaEdicao"}>
+                <Botao> <img src="/icons/plus-icon.svg" alt="Mais" /> Criar Nova edição</Botao>
+            </Link>
+            <section className="w-full py-10 flex flex-col justify-center a">
+                <ButtonCard texto="Interclasse 2026" status="Em andamento" />
+                <ButtonCard texto="Interclasse 2025" status="Finalizado" />
+                <ButtonCard texto="Interclasse 2024" status="Finalizado" />
+                <ButtonCard texto="Interclasse - Volta as Aulas" status="Finalizado" />
+            </section>
+            <NavBar />
         </>
     )
 }
