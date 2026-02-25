a9b0724664d9bccf6d63519062465a9bde62348e
Geovana Eduarda <geovana.e.oliveira@aluno.senai.br>
2026-02-25 14:40:58 -0300

    atualização do prazo.php

--- backend/api/prazo.php
+++ backend/api/prazo.php
@@ -1,6 +1,24 @@
 <?php
-if (date('d/m') == '01/01') {
-    $pasta = __DIR__ .'../../docs/lista_alunos';
-    
+if (date('d/m') == '25/02') {
+
+    $pasta = __DIR__ . '/../../docs/lista_alunos/';
+
+    if (is_dir($pasta)) {
+
+        $arquivos = scandir($pasta);
+
+        foreach ($arquivos as $arquivo) {
+
+            if ($arquivo != "." && $arquivo != "..") {
+
+                $caminhoCompleto = $pasta . $arquivo;
+
+                if (is_file($caminhoCompleto)) {
+                    unlink($caminhoCompleto);
+                }
+            }
+        }
+        echo "Succuess";
+    }
 }
 ?>
