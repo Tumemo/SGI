4b86dc20933f9cd9332e979fa27804ffc067907f
Hugo Silva <hugo.f.silva15@aluno.senai.br>
2026-03-18 13:13:09 -0300

    Atualizações:
    Documentação da página home

--- views/src/pages/home.php
+++ views/src/pages/home.php
@@ -4,57 +4,29 @@ $textTop = '';
 require_once '../components/header.php';
 ?>
 
-<main class="px-4 mt-4">
+<!-- Página Home, onde terá as edições de interclasse -->
+<main class="px-4 mt-4 py-5">
+    <!-- Botão para criar uma nova Edição de interclasse -->
     <a href="./novaEdicao.php" class="btn btn-danger p-2 mb-3"><i class="bi bi-plus-circle me-2"></i>Criar Nova Adição</a>
+    <!-- Lista onde ficará todas as edições, e seus respectivos status -->
     <section id="listInter">
 
-
-        <!-- ===================================================================== -->
-        <!-- dados estaticos utilizado antes de fazer a integração com a API -->
-        <!-- ===================================================================== -->
-
-
-        <a href="./edicao.php" class="text-decoration-none text-black">
-             <div class="d-flex justify-content-between align-items-center shadow p-3 w-100 mb-3">
-                <div>
-                    <h2 class="fs-2">Interclasse 2026</h2>
-                    <h3 class="fs-6 text-secondary">(Em Andamamento)</h3>
-                </div>
-                <div>
-                    <img src="../../public/icons/arrow.svg" alt="icone de seta">
-                </div>
-            </div>
-        </a>
-        <div class="d-flex justify-content-between align-items-center shadow p-3 w-100 mb-3">
-            <div>
-                <h2 class="fs-2">Interclasse 2025</h2>
-                <h3 class="fs-6 text-secondary">(Finalizado)</h3>
-            </div>
-            <div>
-                <img src="../../public/icons/arrow.svg" alt="icone de seta">
-            </div>
-        </div>
-        <div class="d-flex justify-content-between align-items-center shadow p-3 w-100 mb-3">
-            <div>
-                <h2 class="fs-2">Interclasse 2024</h2>
-                <h3 class="fs-6 text-secondary">(Finalizado)</h3>
-            </div>
-            <div>
-                <img src="../../public/icons/arrow.svg" alt="icone de seta">
-            </div>
-        </div>
     </section>
 </main>
 
 
-<!-- <script>
+<script>
+    // Função para listar Interclasse
     async function listarInterclasses() {
         try {
+            // Faz a chamada para API Interclasse
             const url = await fetch('../../../api/view/interclasse.php');
+            // Converte  a resposta da API para JSON
             const res = await url.json();
-            const obj = document.getElementById('listInter');
-
-            obj.innerHTML = res.map((item, key) => (
+            // Pega o elemento HTML com a classe listInter
+            const listaInterclasse = document.getElementById('listInter');
+            // Lista todas interclasses de forma dinamica
+            listaInterclasse.innerHTML = res.map((item, key) => (
                 `
             <a href="./edicao.php" class="text-decoration-none text-black" ${key.id_interclasse}>
                 <div class="d-flex justify-content-between align-items-center shadow p-3 w-100 mb-3 rounded">
@@ -70,13 +42,14 @@ require_once '../components/header.php';
             `
             )).join('')
         } catch (error) {
+            // Mostra o erro no console e no site aparece um erro em velho
             console.log("Erro: ".error)
             document.getElementById('listInter').innerHTML = `<p class="text-danger fs-4 m-auto">Erro ao carregar os Interclasses!!</p>`
         }
     }
-
+    // Chama a função listarInterclasse
     listarInterclasses()
-</script> -->
+</script>
 
 <?php
 require_once '../components/navbar.php'
