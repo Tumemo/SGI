39d8c31418431d225a75c2ba2404d9c6feac1ced
Hugo Silva <hugo.f.silva15@aluno.senai.br>
2026-03-27 13:55:09 -0300

    Atualização:
    Criação da pagina edição turmas, e ajuste de alguns links

---
+++ views/src/pages/edicao_turmas.php
@@ -0,0 +1,35 @@
+<?php
+$titulo = "Turmas";
+$textTop = "Turmas";
+$btnVoltar = true;
+require_once '../componentes/header.php';
+?>
+
+<main>
+    <p class="text-secondary text-center my-3">Editar detalhes turmas</p>
+    <div class="d-flex m-auto justify-content-between align-items-center shadow py-3 px-4 mb-3" style="width: 80%;">
+        <h2 class="m-0 fs-3">1 º ano EM</h2>
+        <picture>
+            <img src="../../public/icons/arrow-right.svg" alt="Seta para direita">
+        </picture>
+    </div>
+    <div class="d-flex m-auto justify-content-between align-items-center shadow py-3 px-4 mb-3" style="width: 80%;">
+        <h2 class="m-0 fs-3">2 º ano EM</h2>
+        <picture>
+            <img src="../../public/icons/arrow-right.svg" alt="Seta para direita">
+        </picture>
+    </div>
+    <div class="d-flex m-auto justify-content-between align-items-center shadow py-3 px-4 mb-3" style="width: 80%;">
+        <h2 class="m-0 fs-3">3 º ano EM</h2>
+        <picture>
+            <img src="../../public/icons/arrow-right.svg" alt="Seta para direita">
+        </picture>
+    </div>
+
+</main>
+
+
+<?php
+
+require_once '../componentes/navbar.php';
+?>
--- views/src/pages/home.php
+++ views/src/pages/home.php
@@ -7,10 +7,17 @@ require_once '../componentes/header.php';
 
 <main>
     <div class="m-auto" style="width: 80%;">
-        <a href="./novaEdiçao.php" class="btn btn-danger d-flex gap-2 mt-3" style="width: max-content;"><i class="bi bi-plus-circle"></i>Criar Nova Edição</a>
+        <a href="./novaEdicao.php" class="btn btn-danger d-flex gap-2 mt-3" style="width: max-content;"><i class="bi bi-plus-circle"></i>Criar Nova Edição</a>
     </div>
-    <section class="m-auto shadow d-flex justify-content-between align-content-center px-3 py-4 rounded-3 my-3" style="width: 80%;" id="listarInterclasses">
-    </section>
+    <div class="m-auto shadow d-flex justify-content-between align-content-center px-3 py-3 rounded-3 my-3" style="width: 80%;">
+        <a href="./edicao.php" class="text-decoration-none text-dark">
+            <div>
+                <h2 class="m-0">Interclasse ???</h2>
+                <p class="text-secondary m-0">(Em Andamento)</p>
+            </div>
+        </a>
+        <img src="../../public/icons/arrow-right.svg" alt="icone de seta">
+    </div>
 </main>
 
 <script>
